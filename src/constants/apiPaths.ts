7e34a9772ab1d00d8f4ebef22a798c50fbf6b02b
Dzmitry Chervanenka <Dzmitry_Chervanenka@epam.com>
2023-04-29 20:10:16 +0300

    fix urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://uh807vv08h.execute-api.eu-north-1.amazonaws.com/dev",
+  product: "https://s6292wlb56.execute-api.eu-north-1.amazonaws.com/dev",
   order: "https://uh807vv08h.execute-api.eu-north-1.amazonaws.com/dev",
   import: "https://7ao7ydvxqi.execute-api.eu-north-1.amazonaws.com/dev",
-  bff: "https://uh807vv08h.execute-api.eu-north-1.amazonaws.com/dev",
+  bff: "https://s6292wlb56.execute-api.eu-north-1.amazonaws.com/dev",
   cart: "https://uh807vv08h.execute-api.eu-north-1.amazonaws.com/dev",
 };
 
